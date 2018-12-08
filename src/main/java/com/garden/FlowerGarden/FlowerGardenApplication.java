e7ff97ce087d0ce57b6115548da923a29b6af759
Oana Patru <oanapatru@Oanas-MacBook-Pro.local>
2018-12-08 07:26:22 -0700

    initial

---
+++ src/main/java/com/garden/FlowerGarden/FlowerGardenApplication.java
@@ -0,0 +1,12 @@
+package com.garden.FlowerGarden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowerGardenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowerGardenApplication.class, args);
+	}
+}
